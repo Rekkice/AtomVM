4cf733c21c7e5047344336cb9e6cd2308ce5dab4
Peter M <petermm@gmail.com>
2025-12-16 10:07:45 +0100

    Test crypto esp32p4
    Signed-off-by: Peter M <petermm@gmail.com>

--- src/platforms/esp32/test/main/test_main.c
+++ src/platforms/esp32/test/main/test_main.c
@@ -319,14 +319,11 @@ TEST_CASE("test_md5", "[test_run]")
     TEST_ASSERT(ret_value == OK_ATOM);
 }
 
-// Full crypto suite not yet supported on ESP32-P4 simulator.
-#if !CONFIG_IDF_TARGET_ESP32P4
 TEST_CASE("test_crypto", "[test_run]")
 {
     term ret_value = avm_test_case("test_crypto.beam");
     TEST_ASSERT(ret_value == OK_ATOM);
 }
-#endif
 
 TEST_CASE("test_monotonic_time", "[test_run]")
 {
